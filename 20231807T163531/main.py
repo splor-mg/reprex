82646e78b8d4d4a4b24d66967555d01bf423c1ec
hslinhares <henrique.s.linhares@gmail.com>
2023-07-21 11:52:47 -0300

    teste do frictionless validate em path formato multipart

--- 20231807T163531/main.py
+++ 20231807T163531/main.py
@@ -1,7 +1,14 @@
+import frictionless
+from frictionless import Schema, describe
 from frictionless import validate
+from pprint import pprint
+# describe = describe("data-raw/alteracoes-orcamentarias_2021.csv")
+# describe.to_yaml("schemas/alteracoes-orcamentarias.yaml")
 
-report = validate('schemas/roperacional-datapackage.yaml')
+report = validate('schemas/alteracoes-orcamentarias.yaml')
 print(report)
+
+
 
 
 
