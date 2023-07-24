66419b9477c832fe87415bbec237e6344839be06
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-07-24 12:31:03 -0300

    adiciona chamada de resource.infer

--- reprex/20230721T164121/main.py
+++ reprex/20230721T164121/main.py
@@ -4,14 +4,11 @@ pipeline = Pipeline(steps=[
         steps.field_update(name='id', descriptor = {'name': 'pkey', 'title': 'Primary Key'}),
         steps.row_filter(formula='name != "france"'),
         steps.table_write(path='output.csv'),
-        steps.resource_update(name='data', descriptor={'path': 'output.csv', 'data': None}),
+        steps.resource_update(name='data', descriptor={'path': 'output.csv'}),
     ])
 
 resource = Resource(path='https://raw.githubusercontent.com/frictionlessdata/frictionless-py/d6f2552b4fd950f459130eda9cf80ae0b8b4931e/data/transform.csv')
 resource.transform(pipeline)
-resource.to_yaml('resource.yaml')
+resource.infer(stats=True)
 
 print(f'{resource=}')
-print(f'{resource.read_rows()=}')
-print(f'{resource.read_cells()}')
-print(f'{resource.to_view()=}')
