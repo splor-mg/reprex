70ac124cc05414f7125016261e9c99d7bb301346
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2024-07-09 12:52:26 -0300

    add test with sample data

--- reprex/20240709T121555/test_main.py
+++ reprex/20240709T121555/test_main.py
@@ -1,7 +1,6 @@
 from frictionless import Resource
-import pytest
 
-def test_bad_delimiter():
+def test_default_delimiter():
 
     resource = Resource.from_descriptor(
         {
@@ -9,6 +8,55 @@ def test_bad_delimiter():
             "path": "data.csv",
             "format": "csv",
             "encoding": "utf-8",
+            "schema": {
+                "fields": [
+                    {"name": "pkey", "type": "string"},
+                    {"name": "year", "type": "integer"},
+                    {"name": "code", "type": "integer"},
+                    {"name": "desc", "type": "string"},
+                ]
+            },
+        }
+    )
+
+    resource.infer(stats=True)
+
+    assert {control.delimiter for control in resource.dialect.controls} == {'|'}
+
+def test_default_delimiter_sample_data():
+
+    resource = Resource.from_descriptor(
+        {
+            "name": "resource",
+            "path": "sample-data.csv",
+            "format": "csv",
+            "encoding": "utf-8",
+            "schema": {
+                "fields": [
+                    {"name": "pkey", "type": "string"},
+                    {"name": "year", "type": "integer"},
+                    {"name": "code", "type": "integer"},
+                    {"name": "desc", "type": "string"},
+                ]
+            },
+        }
+    )
+
+    resource.infer(stats=True)
+
+    assert {control.delimiter for control in resource.dialect.controls} == {'|'}
+
+def test_explicit_delimiter():
+
+    resource = Resource.from_descriptor(
+        {
+            "name": "resource",
+            "path": "data.csv",
+            "format": "csv",
+            "encoding": "utf-8",
+            "dialect": {
+                    "delimiter": ","
+            },
             "schema": {
                 "fields": [
                     {"name": "pkey", "type": "string"},
