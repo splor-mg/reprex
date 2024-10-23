a73d6b4665c693f4daa5b06eb9dea106c3a7da50
hslinhares <henrique.s.linhares@gmail.com>
2024-10-23 12:53:22 -0300

    altera a criação da lista de tabelas para a função create_tables

--- reprex/20241021T101406/test.R
+++ reprex/20241021T101406/test.R
@@ -1,57 +1,64 @@
-library(dpm)
 
 library(dpm)
 library(data.table)
 library(relatorios)
 library(relatoriosOBZ)
 library(RcppTOML)
-library(jsonlite)
+#library(jsonlit)
+    
+
 
 # ================================================================
 ## data import
 
-execucao <- fread('data/exec.csv')
-rp <- fread('data/rp.csv') 
-credito <- fread('data/credito.csv')
-cota <- fread('data/cota.csv')
-receita <- fread('data/receita.csv')
+# execucao <- read_datapackage('datapackages/execucao/datapackage.json')
+# rp <- read_datapackage('datapackages/rp/datapackage.json') 
+# credito <- read_datapackage('datapackages/credito/datapackage.json')
+# cota <- read_datapackage('datapackages/cota/datapackage.json')
+# receita <- read_datapackage('datapackages/receita/datapackage.json')
+# obz <- read_datapackage('datapackages/obz/datapackage.json')
+# # ================================================================
+# ## config
+# 
+# config <- parseTOML("relationships.toml")
+# 
+# # ======================================================
+# ## tables
+# tables <- list(
+#   execucao = list(
+#     df = execucao,
+#     key = config$keys["chave_execucao"],
+#     drop_columns = NULL
+#   ),
+#   rp = list(
+#     df = execucao,
+#     key = config$keys["chave_execucao"],
+#     drop_columns = NULL
+#   ),
+#   credito = list(
+#     df = credito,
+#     key = config$keys["chave_credito"],
+#     drop_columns = NULL
+#   ),
+#   cota = list(
+#     df = cota,
+#     key = config$keys["chave_credito"],
+#     drop_columns = NULL
+#   ),
+#   receita = list(
+#     df = receita,
+#     key = config$keys["chave_receita"],
+#     drop_columns = NULL
+#   )
+# )
 
 # ================================================================
-## config
-
-config <- parseTOML("relationships.toml")
-
-# ======================================================
-## tables
-tables <- list(
-  execucao = list(
-    df = execucao,
-    key = config$keys["chave_execucao"],
-    drop_columns = NULL
-  ),
-  rp = list(
-    df = execucao,
-    key = config$keys["chave_execucao"],
-    drop_columns = NULL
-  ),
-  credito = list(
-    df = credito,
-    key = config$keys["chave_credito"],
-    drop_columns = NULL
-  ),
-  cota = list(
-    df = cota,
-    key = config$keys["chave_credito"],
-    drop_columns = NULL
-  ),
-  receita = list(
-    df = receita,
-    key = config$keys["chave_receita"],
-    drop_columns = NULL
-  )
-)
+## create_tables substitui a etapa acima
+tables <- create_tables('relationships.toml')
 
 # ================================================================
 ## linktable
 link <- create_linktable(tables, keys = config$keys)
+
+
 
