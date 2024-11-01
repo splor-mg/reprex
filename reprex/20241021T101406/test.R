69033b672eef5a9138cb66798b55700cb4db1b19
hslinhares <henrique.s.linhares@gmail.com>
2024-11-01 16:24:18 -0300

    ajusta reprex para o status atual do pacote dpm

--- reprex/20241021T101406/test.R
+++ reprex/20241021T101406/test.R
@@ -1,9 +1,9 @@
-
-library(dpm)
-library(data.table)
 library(relatorios)
 library(relatoriosOBZ)
 library(RcppTOML)
+library(dpm)
+library(data.table)
+
 #library(jsonlit)
     
 
@@ -11,50 +11,51 @@ library(RcppTOML)
 # ================================================================
 ## data import
 
-# execucao <- read_datapackage('datapackages/execucao/datapackage.json')
-# rp <- read_datapackage('datapackages/rp/datapackage.json') 
-# credito <- read_datapackage('datapackages/credito/datapackage.json')
-# cota <- read_datapackage('datapackages/cota/datapackage.json')
-# receita <- read_datapackage('datapackages/receita/datapackage.json')
-# obz <- read_datapackage('datapackages/obz/datapackage.json')
-# # ================================================================
-# ## config
-# 
-# config <- parseTOML("relationships.toml")
-# 
-# # ======================================================
-# ## tables
-# tables <- list(
-#   execucao = list(
-#     df = execucao,
-#     key = config$keys["chave_execucao"],
-#     drop_columns = NULL
-#   ),
-#   rp = list(
-#     df = execucao,
-#     key = config$keys["chave_execucao"],
-#     drop_columns = NULL
-#   ),
-#   credito = list(
-#     df = credito,
-#     key = config$keys["chave_credito"],
-#     drop_columns = NULL
-#   ),
-#   cota = list(
-#     df = cota,
-#     key = config$keys["chave_credito"],
-#     drop_columns = NULL
-#   ),
-#   receita = list(
-#     df = receita,
-#     key = config$keys["chave_receita"],
-#     drop_columns = NULL
-#   )
-# )
+execucao <- read_datapackage('datapackages/execucao/datapackage.json')
+rp <- read_datapackage('datapackages/rp/datapackage.json')
+credito <- read_datapackage('datapackages/credito/datapackage.json')
+cota <- read_datapackage('datapackages/cota/datapackage.json')
+receita <- read_datapackage('datapackages/receita/datapackage.json')
+obz <- read_datapackage('datapackages/obz/datapackage.json')
+# ================================================================
+## config
+
+config <- parseTOML("relationships.toml")
+
+# ======================================================
+## tables
+tables <- list(
+  execucao = list(
+    df = execucao,
+    key = config$keys["chave_execucao"],
+    drop_columns = NULL
+  ),
+  rp = list(
+    df = execucao,
+    key = config$keys["chave_execucao"],
+    drop_columns = NULL
+  ),
+  credito = list(
+    df = credito,
+    key = config$keys["chave_credito"],
+    drop_columns = NULL
+  ),
+  cota = list(
+    df = cota,
+    key = config$keys["chave_credito"],
+    drop_columns = NULL
+  ),
+  receita = list(
+    df = receita,
+    key = config$keys["chave_receita"],
+    drop_columns = NULL
+  )
+)
 
 # ================================================================
 ## create_tables substitui a etapa acima
-tables <- create_tables('relationships.toml')
+result <- create_tables('relationships.toml')
+link <- create_linktable('relationships.toml')
 
 # ================================================================
 ## linktable
